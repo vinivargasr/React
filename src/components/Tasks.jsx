4540f67e5ebf54d1abd60911433b0c492f7e5516
vinivargasr <166772508+vinivargasr@users.noreply.github.com>
2025-01-22 10:49:55 -0300

    adc novas paginas com react

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,16 @@
 import { ArrowUpRight, Trash2  } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
+    const navigate = useNavigate()
+
+    function onSeeDetailsClick(task) {
+        const query = new URLSearchParams()
+        query.set('title', task.title)
+        query.set('description', task.description)
+        navigate(`/task?${query.toString()}`)
+    }
+
     return (
         <ul className="space-y-4 bg-slate-200 p-6 rounded-md shadow">
             {tasks.map((task) => (
@@ -11,7 +21,8 @@ function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
             >
                 {task.title}
             </button>
-            <button className='bg-slate-400 p-2 rounded-md text-white'>
+            <button onClick={() => onSeeDetailsClick(task)}
+                className='bg-slate-400 p-2 rounded-md text-white'>
                 <ArrowUpRight />
             </button>
             <button
